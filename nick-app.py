3331e6adc9043c0da831b71117b7085192fc0cd6
bronsonnh <81539042+bronsonnh@users.noreply.github.com>
2022-01-04 18:20:46 -0500

    Update nick-app.py

--- nick-app.py
+++ nick-app.py
@@ -6,28 +6,6 @@ import seaborn  as sns
 from PIL import Image
 import psycopg2
 
-# Initialize connection.
-# Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from mytable;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-
 st.set_option('deprecation.showPyplotGlobalUse', False)
 sns.set_style('darkgrid')
 st.set_page_config(layout="wide")
